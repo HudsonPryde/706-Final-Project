125f43d7347e5b718b111029cbf359d252ce4fc8
Michael <michaelrevi19@gmail.com>
2023-04-12 16:25:49 -0400

    Update CytoscapeGraph.tsx

--- final-project/src/CytoscapeGraph.tsx
+++ final-project/src/CytoscapeGraph.tsx
@@ -123,6 +123,8 @@ export const GraphUI: FC = () => {
               Compute Decentralized
             </Button>
             <Button onClick={() => {
+              removeAllHighlight()
+              
               setDijkstra(!isDijkstra)
 
               /* Uncomment when dijkstra algorithm is added
