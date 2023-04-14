3cc0daddc2e1e04fdf9af348bd310e66250e9b73
nwalczyk <nicolejwalczyk@gmail.com>
2023-04-14 14:13:42 -0400

    oop

--- final-project/src/Dijkstra.ts
+++ final-project/src/Dijkstra.ts
@@ -1,92 +1,125 @@
 export interface Edge {
-    from: string;
-    to: string;
-    weight: number;
-    isHighlighted?: boolean;
+  from: string;
+  to: string;
+  weight: number;
+  isHighlighted?: boolean;
+}
+
+export class Graph {
+  private edges: Edge[];
+
+  constructor(edges: Edge[]) {
+    this.edges = edges;
   }
-  
-  export class Graph {
-    private edges: Edge[];
-  
-    constructor(edges: Edge[]) {
-      this.edges = edges;
+
+  private getNodes(): string[] {
+    const nodes = new Set<string>();
+    for (const edge of this.edges) {
+      nodes.add(edge.from);
+      nodes.add(edge.to);
     }
-  
-    private getNodes(): string[] {
-      const nodes = new Set<string>();
-      for (const edge of this.edges) {
-        nodes.add(edge.from);
-        nodes.add(edge.to);
+    return Array.from(nodes);
+  }
+
+  private getNeighbors(node: string): Edge[] {
+    return this.edges.filter((edge) => edge.from === node);
+  }
+
+  private findMinimum(nextNodes: Array<string>, distances: Record<string, number>): string{
+    let min: number = -1
+    let minNode: string = '';
+    for (const n in nextNodes){
+      if (min == -1 || distances[nextNodes[n]] < min) {
+        min = distances[nextNodes[n]];
+        minNode = nextNodes[n];
       }
-      return Array.from(nodes);
     }
-  
-    private getNeighbors(node: string): Edge[] {
-      return this.edges.filter((edge) => edge.from === node);
-    }
-  
-    public bellmanFord(
-      startNode: string,
-      endNode: string
+    return minNode
+  }
+
+
+  public dijkstra(
+    startNode: string,
+    endNode: string
     ): (string | number)[][] {
-      const nodes = this.getNodes();
-      const distances: Record<string, number> = {};
-      const predecessors: Record<string, string> = {};
-  
-      for (const node of nodes) {
-        // algortithm is decentralized, so initial distances are set to infinity
-        distances[node] = Infinity;
-        predecessors[node] = "null";
-      }
-  
-      distances[startNode] = 0;
-  
-      // calculate distances
-      for (let i = 0; i < nodes.length - 1; i++) {
-        for (const node of nodes) {
-          const neighbors = this.getNeighbors(node);
-          for (const neighbor of neighbors) {
-            // if the distance to the neighbor is shorter than the current distance
-            const distance = distances[node] + neighbor.weight;
-            if (distance < distances[neighbor.to]) {
-              // update the distance and the predecessor
-              distances[neighbor.to] = distance;
-              predecessors[neighbor.to] = node;
-            }
-          }
-        }
-      }
-  
-      const path: string[] = [endNode];
-      let current = endNode;
-  
-      while (current !== startNode) {
-        const predecessor = predecessors[current];
-        if (!predecessor) {
-          // no path return empty array
-          return [];
-        }
-        //console.log(current);
-        path.unshift(predecessor);
-        current = predecessor;
-      }
-  
-      return path.map((node) => [node, distances[node]]);
-      // return path;
+    const nodes = this.getNodes();
+    const distances: Record<string, number> = {};
+    const predecessors: Record<string, string> = {};
+    const N: Array<string> = []; // N is the array of definite least-cost path nodes
+    
+    // INITIALIZATION: 
+
+    N.push(startNode);
+
+    // init distances to infinity except when they are direct neighbours of start node
+    const neighbors = this.getNeighbors(startNode)
+
+    for (const node in nodes){
+        distances[nodes[node]] = Infinity;
+        predecessors[nodes[node]] = 'null';        
     }
+
+    for (const node in nodes){
+      if (node in neighbors) {
+        for (const neighbor of neighbors){
+          distances[neighbor.to] =  neighbor.weight;
+          predecessors[neighbor.to] = startNode;
+        }
+      }
+    }
+
+    distances[startNode] = 0;
+    delete predecessors[startNode];
+
+    // MAIN LOOP:
+
+    while (N.length <= this.getNodes().length){
+
+      const nextNodes: Array<string> = nodes.filter(next => N.indexOf(next) == -1 && distances[next] < Infinity)
+      const minNode: string = this.findMinimum(nextNodes, distances)
+
+      const neighbors = this.getNeighbors(minNode)
+        
+      for (const neighbor of neighbors) {
+        // check all neighbours of the new node;
+        const distance = distances[minNode] + neighbor.weight;
+        if (distance < distances[neighbor.to]) {
+          // update the distance and the predecessor if new dist is less than previous
+          distances[neighbor.to] = distance;
+          predecessors[neighbor.to] = minNode;
+        }
+      }
+      N.push(minNode);
+      }
+
+    const path: string[] = [endNode];
+    let current = endNode;
+
+    while (current !== startNode) {
+      const predecessor = predecessors[current];
+      if (!predecessor) {
+        // no path return empty array
+        return [];
+      }
+      path.unshift(predecessor);
+      current = predecessor;
+    }
+    return path.map((node) => [node, distances[node]]);
   }
-  
-  // Usage
-  // const edges: Edge[] = [
-  //   { from: 'A', to: 'B', weight: 5 },
-  //   { from: 'A', to: 'C', weight: 2 },
-  //   { from: 'C', to: 'D', weight: 1 },
-  //   { from: 'D', to: 'B', weight: 1 },
-  //   { from: 'A', to: 'E', weight: 3 },
-  //   { from: 'E', to: 'B', weight: 2 },
-  // ];
-  
-  // const graph = new Graph(edges);
-  // const shortestPath = graph.bellmanFord('A', 'B');
-  // console.log(shortestPath); // [ 3, 5 ]
-  
+} 
+// Usage
+// const edges: Edge[] = [
+//   { from: 'A', to: 'B', weight: 5 },
+//   { from: 'D', to: 'B', weight: 1 },
+//   { from: 'A', to: 'D', weight: 3 },
+//   { from: 'B', to: 'C', weight: 6 },
+//   { from: 'C', to: 'D', weight: 1 },
+//   { from: 'E', to: 'B', weight: 2 },
+//   { from: 'C', to: 'E', weight: 10 },
+//   { from: 'D', to: 'E', weight: 1 },
+//   { from: 'E', to: 'C', weight: 1},
+// ];
+
+// const graph = new Graph(edges);
+// const shortestPath = graph.dijkstra('D', 'C');
+// console.log(shortestPath); // [ 3, 5 ]
